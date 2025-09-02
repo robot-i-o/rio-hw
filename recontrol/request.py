91dfe11273c54033ef7801e45c570aba2d31d5f6
etaoxing <etaoxing@users.noreply.github.com>
2025-09-02 15:40:52 -0400

    Add `request.py`

---
+++ recontrol/request.py
@@ -0,0 +1,16 @@
+import uuid
+from dataclasses import dataclass
+from enum import Enum
+
+
+@dataclass
+class Request:
+    """Generic request container."""
+
+    type: Enum
+    params: dict = None
+    id: str = None
+
+    def __post_init__(self):
+        if self.id is None:
+            self.id = str(uuid.uuid4())
