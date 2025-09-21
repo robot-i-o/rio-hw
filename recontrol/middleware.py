595b57130a99f8570271cc7b6c7d4f623303f9fd
etaoxing <etaoxing@users.noreply.github.com>
2025-09-21 01:37:58 -0400

    Improve middleware factory

--- recontrol/middleware.py
+++ recontrol/middleware.py
@@ -1,3 +1,4 @@
+import inspect
 import multiprocessing as mp
 from collections.abc import Callable
 
@@ -5,28 +6,40 @@ from . import middlewares
 from .middlewares import Node
 from .serializers import CloudpickleSerializer
 
+SERVERLESS_MW = ("Shm", "Thread")
+
+
+def class_factory(name, bases, module, attrs=None):
+    Cls = type(name, bases, {})
+    Cls.__module__ = module.__name__
+    # setattr(module, name, Cls)  # register new class for module
+    if attrs is not None:
+        for attr, value in attrs.items():
+            setattr(Cls, attr, value)
+    return Cls
+
 
 def ServerFactory(mw, _Node, *args, **kwargs):
     # dynamic inheritance
     MwCls = getattr(middlewares, f"{mw}Server")
-    if mw in ("Shm", "Thread"):
-        Cls = type(f"{mw}Server", (MwCls,), {})
-        for attr in ("__api__", "__pub__", "__req__"):
-            setattr(Cls, attr, getattr(_Node, attr))
+    Module = inspect.getmodule(_Node)
+    if mw in SERVERLESS_MW:
+        attrs = {k: getattr(_Node, k) for k in ("__api__", "__pub__", "__req__")}
+        Cls = class_factory(f"{_Node.__name__}{mw}Server", (MwCls,), Module, attrs)
     else:
-        Cls = type(f"{mw}Server", (_Node, MwCls), {})
+        Cls = class_factory(f"{_Node.__name__}{mw}Server", (_Node, MwCls), Module)
     return Cls(*args, **kwargs)
 
 
 def ClientFactory(mw, _Node, *args, **kwargs):
     # dynamic inheritance
     MwCls = getattr(middlewares, f"{mw}Client")
-    if mw in ("Shm", "Thread"):
-        Cls = type(f"{mw}Client", (_Node, MwCls), {})
+    Module = inspect.getmodule(_Node)
+    if mw in SERVERLESS_MW:
+        Cls = class_factory(f"{_Node.__name__}{mw}Client", (_Node, MwCls), Module)
     else:
-        Cls = type(f"{mw}Client", (MwCls,), {})
-        for attr in ("__api__", "__pub__", "__req__"):
-            setattr(Cls, attr, getattr(_Node, attr))
+        attrs = {k: getattr(_Node, k) for k in ("__api__", "__pub__", "__req__")}
+        Cls = class_factory(f"{_Node.__name__}{mw}Client", (MwCls,), Module, attrs)
     return Cls(*args, **kwargs)
 
 
@@ -34,7 +47,8 @@ class ServerManager(Node):
     """Helper class to manage multiple servers, each in its own separate process."""
 
     def __init__(self, mw: str, server_fns: list[Callable], start_method: str = "spawn", timeout: float = 5.0):
-        if mw in ("Shm", "Thread"):
+        if mw in SERVERLESS_MW:
+            [fn() for fn in server_fns]
             server_fns = []
         self.server_fns = server_fns
         self.start_method = start_method
