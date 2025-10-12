ebf9e61e67a53808c31ce716fdb3c2331632610f
etaoxing <etaoxing@users.noreply.github.com>
2025-10-12 16:54:36 -0400

    Fix `XArmSocket` staticmethod typing

--- recontrol/robots/xarm.py
+++ recontrol/robots/xarm.py
@@ -405,9 +405,11 @@ class XArmSocket:
     PORT = 30000
     FREQ = 250
 
+    @staticmethod
     def bytes_to_fp32(bytes_data, is_big_endian=False):
         return struct.unpack(">f" if is_big_endian else "<f", bytes_data)[0]
 
+    @staticmethod
     def bytes_to_fp32_list(bytes_data, n=0, is_big_endian=False):
         ret = []
         count = n if n > 0 else len(bytes_data) // 4
@@ -415,24 +417,29 @@ class XArmSocket:
             ret.append(XArmSocket.bytes_to_fp32(bytes_data[i * 4 : i * 4 + 4], is_big_endian))
         return ret
 
+    @staticmethod
     def bytes_to_u8(data):
         data_u8_0 = int.from_bytes(data[:], byteorder="little")
         return data_u8_0
 
+    @staticmethod
     def bytes_to_u16(data):
         data_u16 = data[0] << 8 | data[1]
         return data_u16
 
+    @staticmethod
     def bytes_to_u32(data):
         data_u32 = data[0] << 24 | data[1] << 16 | data[2] << 8 | data[3]
         return data_u32
 
+    @staticmethod
     def bytes_to_u64(data):
         if len(data) != 8:
             raise ValueError("Input data must be exactly 8 bytes long")
         u64 = struct.unpack(">Q", data)[0]
         return u64
 
+    @staticmethod
     def bytes_to_state(data):
         state = {}
         for key, (start, end) in XArmSocket.P30000.items():
